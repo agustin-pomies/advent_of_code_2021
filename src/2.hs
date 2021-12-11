b8a9f60f44dd52154fc746518baec2ce834da4c1
Agustín Pomies <apomies28@gmail.com>
2021-12-10 21:54:05 -0300

    Day 2 - Part 2

--- src/2.hs
+++ src/2.hs
@@ -1,4 +1,4 @@
-import Data.List
+import Prelude hiding (product) 
 
 -- Input & Output
 basePath :: String
@@ -27,10 +27,30 @@ processCommand command =
 processInput :: [String] -> IO [Movement]
 processInput filedata = return $ map processCommand filedata
 
-solve :: [Movement] -> IO Int
-solve movements = return $ let final_x = sum $ map fst movements
-                               final_y = sum $ map snd movements
-                           in final_x * final_y
+solveWithXY :: [Movement] -> IO Int
+solveWithXY movements = return $ let final_x = sum $ map fst movements
+                                     final_y = sum $ map snd movements
+                                 in final_x * final_y
+
+type Aim = Int
+type Position = (Int, Int)
+type Submarine = (Aim, Position)
+
+modifySubmarine :: Submarine -> Movement -> Submarine
+modifySubmarine (aim, position) (0, units)    = (aim + units, position)
+modifySubmarine (aim, (x, y))   (units, 0)    = (aim, (x + units, y + aim * units))
+
+product :: Position -> Int
+product pair = fst pair * snd pair
+
+solveWithAim :: [Movement] -> IO Int
+solveWithAim movements = return $ product . snd $ foldl modifySubmarine (0,(0,0)) movements
+
+data Strategy = XY | Aim
+
+solveWith :: Strategy -> [Movement] -> IO Int
+solveWith XY  = solveWithXY
+solveWith Aim = solveWithAim
 
 -- Main Program
 main :: IO ()
@@ -38,10 +58,21 @@ main = do
   putStrLn $ "Solving test data..."
   filedata <- readInput testPath
   problemData <- processInput filedata
-  answer <- solve problemData
-  putStrLn $ "Test answer is " ++ (show answer)
+  answer <- solveWith XY problemData
+  putStrLn $ "Test answer using XY is " ++ (show answer)
   putStrLn $ "Solving Part 1..."
   filedata <- readInput inputPath
   problemData <- processInput filedata
-  answer <- solve problemData
+  answer <- solveWith XY problemData
   putStrLn $ "Part 1 answer is " ++ (show answer)
+  putStrLn $ "* * * * * * * * *"
+  putStrLn $ "Solving test data..."
+  filedata <- readInput testPath
+  problemData <- processInput filedata
+  answer <- solveWith Aim problemData
+  putStrLn $ "Test answer using Aim is " ++ (show answer)
+  putStrLn $ "Solving Part 2..."
+  filedata <- readInput inputPath
+  problemData <- processInput filedata
+  answer <- solveWith Aim problemData
+  putStrLn $ "Part 2 answer is " ++ (show answer)
