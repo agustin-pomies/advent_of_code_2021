34a3a6b5723a9d11c0ea58c09fb28ee862443aea
Agustín Pomies <apomies28@gmail.com>
2021-12-12 21:10:06 -0300

    Day 3 - Part 2

--- src/3.hs
+++ src/3.hs
@@ -33,23 +33,44 @@ leastCommon = snd . minimum . map (\xs -> (length xs, head xs)) . group . sort
 binToDec :: Binary -> Int
 binToDec = foldl (\x y -> fromEnum y + 2*x) 0
 
-solve :: Binaries -> IO Int
-solve binaries = return $ let columns = transpose binaries
-                              gammaRate = binToDec (map mostCommon columns)
-                              epsilonRate = binToDec (map leastCommon columns)
-                              powerConsumption = gammaRate * epsilonRate
-                          in powerConsumption
+powerConsumption :: Binaries -> IO Int
+powerConsumption binaries = return $ let columns = transpose binaries
+                                         gammaRate = binToDec (map mostCommon columns)
+                                         epsilonRate = binToDec (map leastCommon columns)
+                                     in gammaRate * epsilonRate
+
+reduceOptions :: Binaries -> (Binary -> Bool) -> Int -> Binary
+reduceOptions [binary] _ _    = binary
+reduceOptions binaries f pos  = let column = map (!! pos) binaries
+                                    examinedDigit = f column
+                                in reduceOptions (filter ((== examinedDigit) . (!!pos)) binaries) f (pos + 1)
+
+lifeSupportRating :: Binaries -> IO Int
+lifeSupportRating binaries = return $ let oxygenGeneratorRating = binToDec $ reduceOptions binaries mostCommon 0
+                                          co2ScrubberRating = binToDec $ reduceOptions binaries leastCommon 0
+                                      in co2ScrubberRating * oxygenGeneratorRating
 
 -- Main Program
 main :: IO ()
 main = do
   putStrLn $ "Solving test data..."
   filedata <- readInput testPath
-  problemData <- processInput filedata
-  answer <- solve problemData
-  putStrLn $ "Test answer is " ++ (show answer)
+  report <- processInput filedata
+  answer <- powerConsumption report
+  putStrLn $ "Power Consumption from example is " ++ (show answer)
   putStrLn $ "Solving Part 1..."
   filedata <- readInput inputPath
-  problemData <- processInput filedata
-  answer <- solve problemData
-  putStrLn $ "Part 1 answer is " ++ (show answer)
+  report <- processInput filedata
+  answer <- powerConsumption report
+  putStrLn $ "Power Consumption is " ++ (show answer)
+  putStrLn $ "* * * * * * * * *"
+  putStrLn $ "Solving test data..."
+  filedata <- readInput testPath
+  report <- processInput filedata
+  answer <- lifeSupportRating report
+  putStrLn $ "Life Support Rating from example is " ++ (show answer)
+  putStrLn $ "Solving Part 2..."
+  filedata <- readInput inputPath
+  report <- processInput filedata
+  answer <- lifeSupportRating report
+  putStrLn $ "Life Support Rating is " ++ (show answer)
