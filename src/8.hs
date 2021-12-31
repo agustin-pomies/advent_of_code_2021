5045721e9808e9fa8249e73bac6645cff845d9a8
Agustín Pomies <apomies28@gmail.com>
2021-12-30 22:39:07 -0300

    Day 8 - Part 2

--- src/8.hs
+++ src/8.hs
@@ -1,5 +1,8 @@
 module Day8 where
+  import Data.List((\\), delete, groupBy, intersect, sort)
   import Data.List.Split (splitOn)
+  import Data.Map (Map, (!))
+  import qualified Data.Map as Map
 
   -- Input & Output
   basePath :: String
@@ -29,14 +32,84 @@ module Day8 where
   processInput filedata = return $ map processLine filedata
 
   -- Logic Handling
-  easyDigitDisplay :: [Int]
-  easyDigitDisplay = [2, 3, 4, 7]
+  digitPatterns :: Map Signal Int
+  digitPatterns = Map.fromList [
+      ("cf", 1),      -- 2
+      ("acf", 7),     -- 3
+      ("bcdf", 4),    -- 4
+      ("acdeg", 2),   -- 5
+      ("acdfg", 3),
+      ("abdfg", 5),
+      ("abcefg", 0),  -- 6
+      ("abdefg", 6),
+      ("abcdfg", 9),
+      ("abcdefg", 8)  -- 7
+    ]
+
+  numSegmentsUsed :: Map Int [Int]
+  numSegmentsUsed = Map.mapKeysWith (++) length (Map.map ((flip (:)) []) digitPatterns)
 
   isEasyDigit :: Signal -> Bool
-  isEasyDigit pattern = elem (length pattern) easyDigitDisplay
+  isEasyDigit = ((== 1) . length . ((!) numSegmentsUsed) . length)
+
+  countEasyDigits :: Notes -> IO Int
+  countEasyDigits notes = return $ (length . (filter isEasyDigit) . concat . (map snd)) notes
+
+  selectPattern :: Int -> Patterns -> Signal
+  selectPattern num = head . (filter ((== num) . length))
+
+  isSublistOf :: (Eq a) => [a] -> [a] -> Bool
+  l1 `isSublistOf` l2 = intersect l1 l2 == l1
+
+  type WireCrossing = Map Char Char
+
+  educatedDeduction :: Patterns -> WireCrossing
+  educatedDeduction patterns =
+    let noisySeven              = selectPattern 3 patterns
+        noisyOne                = selectPattern 2 patterns
+        noisyTopSegment         = head (noisySeven \\ noisyOne)
+        noisyFour               = selectPattern 4 patterns
+        noisySixSegments        = filter ((== 6) . length) patterns
+        noisyNine               = (head . (filter (noisyFour `isSublistOf`))) noisySixSegments
+        noisyFiveSegments       = filter ((== 5) . length) patterns
+        noisyCenterSegment      = head (foldl1 intersect (noisyFour : noisyFiveSegments))
+        noisyBottomLeftSegment  = head (foldl1 intersect (delete noisyNine noisySixSegments) \\ noisyNine)
+        noisyBottomSegment      = head ((foldl1 intersect noisyFiveSegments) \\ [noisyCenterSegment, noisyTopSegment])
+        incompleteTwo           = [noisyTopSegment, noisyCenterSegment, noisyBottomLeftSegment, noisyBottomSegment]
+        noisyTopRightSegment    = head (head (filter ((== 1) . length) (map ((flip (\\)) incompleteTwo) noisyFiveSegments)))
+        noisyTwo                = noisyTopRightSegment : incompleteTwo
+        noisyThree              = head (filter ((== 1) . length . ((flip (\\)) noisyTwo)) noisyFiveSegments)
+        noisyBottomRightSegment = head (noisyThree \\ noisyTwo)
+        noisyTopLeftSegment     = head (noisyNine \\ noisyThree)
+    in Map.fromList [
+      (noisyTopSegment,         'a'),
+      (noisyTopLeftSegment,     'b'),
+      (noisyTopRightSegment,    'c'),
+      (noisyCenterSegment,      'd'),
+      (noisyBottomLeftSegment,  'e'),
+      (noisyBottomRightSegment, 'f'),
+      (noisyBottomSegment,      'g')
+    ]
+
+  deduceWireCrossing :: Patterns -> WireCrossing
+  deduceWireCrossing = educatedDeduction
+
+  signalToDigit :: WireCrossing -> Signal -> Int
+  signalToDigit mapping noisySignal = let signal = map ((!) mapping) noisySignal
+                                      in (((!) digitPatterns) . sort) signal
+
+  undigits :: Integral n => [n] -> n
+  undigits = foldl (\a b -> a * 10 + b) 0
+
+  patternsToOutput :: WireCrossing -> Patterns -> Int
+  patternsToOutput mapping patterns = undigits $ map (signalToDigit mapping) patterns
+
+  entryOutput :: Entry -> Int
+  entryOutput (sample, noisyOutput) = let mapping = deduceWireCrossing sample
+                                      in patternsToOutput mapping noisyOutput
 
   solve :: Notes -> IO Int
-  solve pattern = return $ (length . (filter isEasyDigit) . concat . (map snd)) pattern
+  solve notes = return $ sum (map entryOutput notes)
 
   -- Main Program
   part1 :: IO ()
@@ -44,12 +117,18 @@ module Day8 where
     putStrLn $ "Solving Part 1..."
     filedata <- readInput inputPath
     problemData <- processInput filedata
-    answer <- solve problemData
+    answer <- countEasyDigits problemData
     putStrLn $ "Numbers 1, 4, 7 and 8 appear " ++ (show answer) ++ " times"
 
   part2 :: IO ()
-  part2 = undefined
+  part2 = do
+    putStrLn $ "Solving Part 2..."
+    filedata <- readInput inputPath
+    problemData <- processInput filedata
+    answer <- solve problemData
+    putStrLn $ "Sum of output numbers is " ++ (show answer)
 
   main :: IO ()
   main = do
     part1
+    part2
--- tests/8.test.hs
+++ tests/8.test.hs
@@ -1,14 +1,32 @@
+import Day8 hiding (main)
+
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 import Test.Hspec
 
--- Generators --
--- Add more generators...
-
--- Props --
--- Add more props...
-
--- Main Test Module --
+-- Example --
 testPath :: String
 testPath = basePath ++ "examples/8.txt"
 
-main = undefined
+-- Generators --
+smallExample :: Entry
+smallExample = let sample = ["acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"]
+                   output = ["cdfeb", "fcadb", "cdfeb", "cdbaf"]
+               in (sample, output)
+
+-- Props --
+prop_smallExample :: Bool
+prop_smallExample = entryOutput smallExample == 5353
+
+prop_largerExample :: Property
+prop_largerExample = monadicIO $ do
+  filedata <- run $ readInput testPath
+  noisyNotes <- run $ processInput filedata
+  assert $ (map entryOutput noisyNotes) == [8394, 9781, 1197, 9361, 4873, 8418, 4548, 1625, 8717, 4315]
+
+-- Main Test Module --
+main = hspec $ do
+  describe "Seven Segment Search - Part 2" $
+    do
+      it "prop_smallExample" $ property prop_smallExample
+      it "prop_largerExample" $ property prop_largerExample
